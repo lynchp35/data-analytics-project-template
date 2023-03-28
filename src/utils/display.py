1d11a82cf3981d4b660d0affc44c30c790bc079c
lynchp35 <78041488+lynchp35@users.noreply.github.com>
2023-03-28 14:12:04 +0100

    bare bones

--- src/utils/display.py
+++
@@ -1,9 +0,0 @@
-from IPython.core.display import HTML
-from IPython.display import display
-
-def printdf(df):
-  display(HTML(df.to_html()))
-  
-
-def printsf(sf):
-  display(HTML(sf.to_frame().to_html()))
